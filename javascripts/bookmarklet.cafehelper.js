7298af942ed80d9f3cf84681049b70feebb4e3bb
Andrew Walker <contact@awalker.ca>
2014-01-04 11:28:56 -0500

    somehow missed js file

---
+++ javascripts/bookmarklet.cafehelper.js
@@ -0,0 +1,168 @@
+(function(){
+    "use strict";
+    var v = '1.3.2';
+    if (window.jQuery === undefined || window.jQuery.fn.jquery < v){
+        var done = false;
+        var script = document.createElement('script');
+        script.src = 'http://ajax.googleapis.com/ajax/libs/jquery/' + v +
+            '/jquery.min.js';
+        script.onload = script.onreadystatechange = function(){
+            if (!done && (!this.readyState || this.readyState == 'loaded' || this.readyState == 'complete')) {
+                done = true;
+                initMyBookmarklet();
+            }
+        };
+        document.getElementsByTagName('head')[0].appendChild(script);
+    } else {
+        initMyBookmarklet();
+    }
+    var sectionLink = 'viewforum.php?forum=';
+
+    var storage = (function(){
+        localStorage.name = 'magicCafeHelper';
+        return {
+            save: function save(val){
+                localStorage.setItem('selectedVals',val);
+            },
+            load: function load(){
+                return localStorage.getItem('selectedVals');
+            },
+            reset: function reset(){
+                localStorage.clear();
+            },
+            hasStoredValues: function hasStoredValues(){
+                return storage.load() !== null;
+            }
+        }
+    }());
+
+    var getSectionsToDisplay = function getSectionsToDisplay(){
+        var selectedSections = [];
+        $('.allSections :checked').each(function(i,section){
+            selectedSections.push($(section).val());
+        });
+        return selectedSections;
+
+    };
+    var getAllSections = function getAllSections(){
+        var sections = [];
+        $('td a[href^="' + sectionLink + '"]').each(function(i,section){
+            sections.push($(section).text());
+        });
+        return sections;
+    };
+
+    var resetSettings = function resetSettings(evt){
+        evt.preventDefault();
+        evt.stopPropagation();
+        storage.reset();
+        $('.allSections :checked').each(function(i,section){
+            $(section).click();
+        });
+        window.location.reload();
+    };
+    var updateView = function updateView(evt){
+        var header = $('body > table'),
+            footer = $('body > table:last'),
+            sectionsContainer = $('table + table table tbody'),
+            sectionTitlesToDisplay = getSectionsToDisplay();
+        sectionsContainer.find('tr').addClass('cafeHelperHidden');
+        $.each(sectionTitlesToDisplay, function(i,section){
+            $('a[href^="' + sectionLink + '"]:contains(' + section + ')').parents('tr').removeClass('cafeHelperHidden').addClass('cafeHelperVisible');
+        });
+        footer.find('tr').addClass('cafeHelperVisible');
+        storage.save(sectionTitlesToDisplay.join("|^|"));
+        closePanel();
+        evt.preventDefault();
+        evt.stopPropagation();
+    };
+
+    var buildIdFromName = function buildIdFromName(name){return name.toLowerCase().replace(/\s/g,'');};
+
+    var renderPanel = function renderPanel(){
+        var panel = $('<div id=cafeBookmarkletHelper ></div>'),
+            header = $('<h2>CafeHelper</h2>'),
+            body = $('<div class=helperBody></div>'),
+            buttonBar = $('<div class="buttonBar"></div>'),
+            sectionList = $('<ul class=allSections></ul>'),
+            allSections = getAllSections(),
+            resetButton = $('<input type="button" id="cafeHelperReset" value="Reset" />'),
+            runButton = $('<input type="button" id="cafeHelperfixIt" value="Fix It!" />');
+        addStyles();
+        resetButton.click(resetSettings);
+        runButton.click(updateView);
+        header.click(function(){
+            var helper = $("#cafeBookmarkletHelper");
+            if(helper.hasClass('helperExpanded')){
+                closePanel();
+            }else{
+                showPanel();
+            }
+        });
+        $.each(allSections,function(i,section){
+            var line = $('<li><input type=checkbox value="' +
+                section +
+                '" id=' +
+                buildIdFromName(section) +
+                '></input><label for="' +
+                buildIdFromName(section) +
+                '">' +
+                section +
+                '</label></li>');
+            sectionList.append(line);
+        });
+        panel.append(header);
+        buttonBar.append(runButton);
+        buttonBar.append(resetButton);
+        body.append(buttonBar);
+        body.append(sectionList);
+        panel.append(body);
+        $('body').append(panel);
+        $("#cafeBookmarkletHelper").addClass("helperExpanded");
+        if(storage.hasStoredValues()){
+            hidePanel();
+            processSavedValues();
+        }else{
+            showPanel();
+        }
+    };
+
+    var processSavedValues = function processSavedValues(){
+        var savedVals = storage.load().split("|^|");
+        $.each(savedVals, function(i,savedVal){
+            $('.allSections input[value=' + savedVal + ']').click();
+        });
+        $('#cafeBookmarkletHelper #cafeHelperfixIt').click();
+
+    };
+
+    var closePanel = function closePanel(){
+        $('#cafeBookmarkletHelper .helperBody').slideUp('fast', function(){
+            $("#cafeBookmarkletHelper").toggleClass('helperExpanded');
+        });
+    };
+
+    var hidePanel = function hidePanel(){
+        $('#cafeBookmarkletHelper .helperBody').hide();
+    };
+
+    var showPanel = function showPanel(){
+        $('#cafeBookmarkletHelper .helperBody').slideDown('fast', function(){
+            $("#cafeBookmarkletHelper").toggleClass('helperExpanded');
+        });
+    };
+
+    var addStyles = function addStyles(){
+        var link = $('<link />');
+
+        var path = $("#magicCafeBookmarkletScript").attr('data-path') || '';
+        link.attr('href', path + 'bookmarklet.cafehelper.css').attr('media','screen').attr('rel','stylesheet').attr('type','text/css');
+        $('head').append(link);
+    };
+    var initMyBookmarklet = function initMyBookmarklet(){
+        (window.myBookmarklet = function(){
+
+            renderPanel();
+        }());
+    }
+}());
